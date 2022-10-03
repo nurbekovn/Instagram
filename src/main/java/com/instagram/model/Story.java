d996f713959c71d5b7ec932a7b693551511d5569
arlen34 <arlen.kubanychbekovv01@gmail.com>
2022-10-03 21:20:12 +0300

    models and values

--- src/main/java/com/instagram/model/Story.java
+++ src/main/java/com/instagram/model/Story.java
@@ -1,4 +1,20 @@
 package com.instagram.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Time;
+
+@Entity
+@Table(name = "stories")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Story {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private Time created;
 }
