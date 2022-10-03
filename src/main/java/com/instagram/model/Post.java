7de6a8f350ae2433bc6ffed93eabff2f4fd93f76
Nurmuhammad <nur.nurbekov.01@gmail.com>
2022-10-03 21:43:02 +0600

    dto

--- src/main/java/com/instagram/model/Post.java
+++ src/main/java/com/instagram/model/Post.java
@@ -1,5 +1,22 @@
 package com.instagram.model;
 
-public class Post {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String description;
+
+    private String name;
+    
 }
